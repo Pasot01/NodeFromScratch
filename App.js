5b0fb2f6325054b2ffbd691af44f8b39bb019566
Pasot01 <contactmyphone14@gmail.com>
2022-07-23 22:34:38 +0200

    Ajout d'une liste de pokémons requirée dans App et configuration des appels de pokémons par id ainsi qu'un test de calcul du nombre de pokémons

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 const express = require('express')
 
+let pokemons = require('./mock-pokemons')
+
 const app = express()
 
 const port = 3000
 
 // Point de terminaison Express = app.METHODE(CHEMIN,GESTIONNAIRE(req,res))
-app.get('/', (req, res) => res.send('Hello, Express! '))
+// app.get('/', (req, res) => res.send('Hello, Express! '))
+
+app.get('/api/pokemons/', (req, res) => {
+    // const nbPokemons = pokemons.length
+    res.send(`Il y a ${pokemons.length} pokémons pour le moment dans le pokédex.`)
+})
 
 // Récupération d'un id et d'un nom avec params
-app.get('/api/pokemons/:id/:name', (req, res) => {
-    const id = req.params.id 
-    const name= req.params.name
-    res.send(`Le pokémon n°${id} est ${name}.`)
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id) 
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}.`)
 })
 
 
