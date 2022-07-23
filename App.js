dea06cd54cce67ca8356111c003dee126434f7cb
Pasot01 <contactmyphone14@gmail.com>
2022-07-23 21:53:25 +0200

    Ajout du module Nodemon et test de mise en place de routes avec récupération de paramètres id et name sur la barre d'addresse

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
-// Avec Node en console sans Express
-// console.log('Hello, Node!');
-
-// Après installation d'Express 
-// require appel express dans node_modules
 const express = require('express')
 
-// créationd'une instance Express pour notre serveur web
 const app = express()
 
-// port d'utilisation
 const port = 3000
 
-// get = méthode de la requête
-// get/arg 1 : point de terminaison ('/')
-// get/arg 2 : fonction f(2 args) après ('/') trouvé
-// f/arg1 :  : recup obj request == req entrée ('/')
-// f/arg2 :  : obj res == response == obj Express -> client
-// méthode send -> renvoie notre message
-app.get('/', (req, res) => res.send('Hello, Express!'))
+// Point de terminaison Express = app.METHODE(CHEMIN,GESTIONNAIRE(req,res))
+app.get('/', (req, res) => res.send('Hello, Express! '))
 
-// Démarrage de l'API Rest sur le port 3000 et écoute avec la methode listen
+// Récupération d'un id et d'un nom avec params
+app.get('/api/pokemons/:id/:name', (req, res) => {
+    const id = req.params.id 
+    const name= req.params.name
+    res.send(`Le pokémon n°${id} est ${name}.`)
+})
+
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
 
